f9da444e9f7234da3ef5dffa68791e6c3f19d89a
KunYoung Heo <notegrapher@gmail.com>
2017-03-24 23:41:36 +0900

    practice

--- elections/views.py
+++ elections/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Candidate
 
 def index(request):
-	return HttpResponse("Hello Django!")
+	candidates = Candidate.objects.all()
+	str = ""
+	for candidate in candidates:
+		str += "Person {} {} ({})<br>".format(candidate.name, candidate.party_number, candidate.area)
+		str += candidate.introduction + "<P>"
+
+	return HttpResponse(str)
