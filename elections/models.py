8ed7c850400257d3c0cd6b748f7e82a1e2641baf
KunYoung Heo <notegrapher@gmail.com>
2017-03-24 23:18:44 +0900

    practice

---
+++ elections/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Candidate(models.Model):
+	name = models.CharField(max_length=10)
+	introduction = models.TextField()
+	area = models.CharField(max_length=15)
+	party_number = models.IntegerField(default=1)
+  
+	def __str__(self):
+		return self.name
