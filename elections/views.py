7a3c24b49bd807a3f701a008134b0d370df2a653
KunYoung Heo <notegrapher@gmail.com>
2017-03-26 12:56:32 +0900

    practice

--- elections/views.py
+++ elections/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-from .models import Candidate
+from .models import Candidate, Poll, Choice
+from django.utils import timezone
+
 
 def index(request):
 	candidates = Candidate.objects.all()
@@ -12,4 +14,17 @@ def index(request):
 
 
 def areas(request, area):
-	return HttpResponse(area)
+	today = timezone.now()
+	try:
+		poll = Poll.objects.get(area=area, start_date__lte=today, end_date__gte=today)
+		candidates = Candidate.objects.filter(area = area)
+	
+	except:
+		poll = None
+		candidates = None
+
+	context = {'candidates': candidates,
+						 'area': area,
+						 'poll': poll}
+	
+	return render(request, 'elections/area.html', context)
